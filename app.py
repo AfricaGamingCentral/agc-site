52bc386d36e34c87941f2ff001aa7da83f6fd61e
Francois <fbothma93@gmail.com>
2018-07-26 15:52:13 +0200

    Update: - Route update with different routes
    Add:
    - Meme stuff

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/templates/roles.html')
+@app.route('/roles')
 def roles():
 	return render_template('roles.html')
 
 
-@app.route('/templates/stats.html')
+@app.route('/stats')
 def stats():
 	return render_template('stats.html')
 
 
-@app.route('/templates/contact.html')
+@app.route('/contact')
 def contact():
 	return render_template('contact.html')
